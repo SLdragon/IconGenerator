2aeb0669207b77ab0877f27b3ac958872d7eac9c
SLdragon <turenlong@qq.com>
2015-10-22 22:04:11 +0800

    加入了手动输入图片尺寸功能

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.checkbox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textWidth = new System.Windows.Forms.TextBox();
+            this.textHeight = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,22 +45,70 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // checkbox
+            // 
+            this.checkbox.AutoSize = true;
+            this.checkbox.Checked = true;
+            this.checkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkbox.Location = new System.Drawing.Point(108, 76);
+            this.checkbox.Name = "checkbox";
+            this.checkbox.Size = new System.Drawing.Size(72, 16);
+            this.checkbox.TabIndex = 1;
+            this.checkbox.Text = "自动生成";
+            this.checkbox.UseVisualStyleBackColor = true;
+            this.checkbox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(27, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "手动宽高";
+            // 
+            // textWidth
+            // 
+            this.textWidth.Enabled = false;
+            this.textWidth.Location = new System.Drawing.Point(98, 107);
+            this.textWidth.Name = "textWidth";
+            this.textWidth.Size = new System.Drawing.Size(62, 21);
+            this.textWidth.TabIndex = 3;
+            // 
+            // textHeight
+            // 
+            this.textHeight.Enabled = false;
+            this.textHeight.Location = new System.Drawing.Point(178, 107);
+            this.textHeight.Name = "textHeight";
+            this.textHeight.Size = new System.Drawing.Size(62, 21);
+            this.textHeight.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 84);
+            this.ClientSize = new System.Drawing.Size(284, 143);
+            this.Controls.Add(this.textHeight);
+            this.Controls.Add(this.textWidth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkbox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Icon生成工具";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textWidth;
+        private System.Windows.Forms.TextBox textHeight;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -32,30 +32,73 @@ namespace WindowsFormsApplication1
             if (fileDialog.ShowDialog() == DialogResult.OK) 
             { 
                 string file = fileDialog.FileName; 
-                MessageBox.Show("已选择文件:" + file, "选择文件提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 Bitmap img = new Bitmap(file);
-
+                MessageBox.Show("已选择文件:" + file+"\n图片尺寸："+img.Width+"×"+img.Height, "选择文件提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
                
-                SaveFileDialog sfd = new SaveFileDialog();
+                SaveFileDialog sfd = new SaveFileDialog();                
                
                 sfd.Filter = "jpg文件(*.jpg)|*.jpg";
+                Bitmap target;
                 if (sfd.ShowDialog() == DialogResult.OK)
                 {
                     String path = sfd.FileName;
-                    String path36_36=path.Insert(path.Length-4,"36_36");
-                    String path48_48 = path.Insert(path.Length - 4, "48_48");
-                    String path72_72 = path.Insert(path.Length - 4, "72_72");
-                    String path128_128 = path.Insert(path.Length - 4, "128_128");
 
-                    Bitmap target = new Bitmap(img, new Size(36, 36));
-                    target.Save(path36_36);
-                    target = new Bitmap(img, new Size(48, 48));
-                    target.Save(path48_48);
-                    target = new Bitmap(img, new Size(72, 72));
-                    target.Save(path72_72);
-                    target = new Bitmap(img, new Size(128,128));
-                    target.Save(path128_128); 
+                    if (checkbox.Checked)
+                    {
+                        String path36_36 = path.Insert(path.Length - 4, "36_36");
+                        String path48_48 = path.Insert(path.Length - 4, "48_48");
+                        String path72_72 = path.Insert(path.Length - 4, "72_72");
+                        String path128_128 = path.Insert(path.Length - 4, "128_128");
+
+                        target = new Bitmap(img, new Size(36, 36));
+                        target.Save(path36_36);
+                        target = new Bitmap(img, new Size(48, 48));
+                        target.Save(path48_48);
+                        target = new Bitmap(img, new Size(72, 72));
+                        target.Save(path72_72);
+                        target = new Bitmap(img, new Size(128, 128));
+                        target.Save(path128_128);
+                    }
+                    else
+                    {
+                        String newPath = path.Insert(path.Length - 4, "new");
+                        int width, height;
+                        int.TryParse(textWidth.Text, out width);
+                        int.TryParse(textHeight.Text, out height);
+
+                        if (width == 0)
+                        {
+                            width = img.Width;
+                            textWidth.Text = img.Width.ToString();
+                        }
+                        if (height == 0)
+                        {
+                            height = img.Height;
+                            textHeight.Text = img.Height.ToString();
+                        }
+                        
+                        target = new Bitmap(img, new Size(width, height));
+                        target.Save(newPath);
+                    }
+
                 }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkbox.Checked)
+            {
+                textWidth.Enabled = false;
+                textHeight.Enabled = false;
+                label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            }
+            else
+            {
+                textWidth.Enabled = true;
+                textHeight.Enabled = true;
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
             }
         }
     }
